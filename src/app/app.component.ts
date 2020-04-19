9e9f4c51cd9d4502f9a88634e78a4c19549bf6f9
wangjiahui1 <915487924@qq.com>
2020-04-19 11:21:43 +0800

    第三次

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ch0401';
+
+  shouldShow = false;
+
+  score = 70;
+
+  color: string;
+
+  fontSize: string;
+
+  isBordered = true;
+
+  classObj = {
+    borderer: this.isBordered,
+    blue: false,
+    round: true
+  }
+
+  onToggle(){
+     this.shouldShow = !this.shouldShow;
+     return false;
+  }
+
+  grade() {
+    return Math.floor(this.score/10);
+  }
+
+  onInput(evt: Event){
+    const inputEle = <HTMLInputElement>evt.target;
+    this.score = Number(inputEle.value);
+  }
+
+  apply(color: string, fontSize: string) {
+    this.color = color;
+    this.fontSize = fontSize;
+  }
+
+  toggleBorder() {
+    this.isBordered = !this.isBordered;
+  }
+
 }
